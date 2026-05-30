a2cb90f1b75afb2e96d2761add79b16437e3f1fe
Will <williamok221@gmail.com>
2026-05-30 13:13:05 +0100

    Add firebase authentication
    * Added basic firebase login
    
    * Added firebase login more stuff

---
+++ authfunctions.py
@@ -0,0 +1,49 @@
+import os
+import pyrebase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+config = {
+    "apiKey": os.getenv("apiKey"),
+    "authDomain": os.getenv("authDomain"),
+    "projectId": os.getenv("projectId"),
+    "storageBucket": os.getenv("storageBucket"),
+    "messagingSenderId": os.getenv("messagingSenderId"),
+    "appId": os.getenv("appId"),
+    "measurementId": os.getenv("measurementId"),
+    "databaseURL": ''
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+#email = 'test@willcodes.tech'
+#password = '123456'
+
+def login_email_password(email,password):
+    user = auth.sign_in_with_email_and_password(email,password)
+    return user
+
+def send_password_reset(email):
+    print(auth.send_password_reset_email(email))
+    return 'sent'
+
+def create_account_email_password(email,password):
+    user = auth.create_user_with_email_and_password(email, password)
+    return user
+
+#user = auth.create_user_with_email_and_password(email, password)
+#print(user)
+
+#user = auth.sign_in_with_email_and_password(email,password)
+#info = auth.get_account_info(user['idToken'])
+#print(info)
+
+
+#auth.send_email_verification(user['idToken'])
+
+#auth.send_password_reset_email(email)
+
+if __name__ == '__main__':
+    print('Do not run this script directly!')
--- main.py
+++ main.py
@@ -1,24 +1,60 @@
 from flask import *
 from dotenv import load_dotenv
+import os
+from authfunctions import *
 
 load_dotenv()
+
 app = Flask(__name__)
+
+app.secret_key = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def home():
-    return render_template(
-        'index.html',
-        styles=url_for('static', filename='dist/output.css'),
-        plantimg=url_for('static', filename='plant.png')
+    if('user' in session):
+        return render_template(
+            'index.html',
+            styles=url_for('static', filename='tailwindstyles.css'),
+            plantimg=url_for('static', filename='plant.png'),
+            loggedin=True,
+            email=session['user']
         )
+    else:
+        return render_template(
+            'index.html',
+            styles=url_for('static', filename='tailwindstyles.css'),
+            plantimg=url_for('static', filename='plant.png'),
+            loggedin=False
+        )
+    
 
 
-@app.route('/login')
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if('user' in session):
+        return redirect('/')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            login_email_password(email,password)
+            session['user'] = email
+        except:
+            return 'Failed to login'
+
     return render_template(
         'login.html',
-        styles=url_for('static', filename='tailwindstyles.css'),
+        styles=url_for('static', filename='login-style.css'),
         )
+
+@app.route('/logout')
+def logout():
+    if('user' in session):
+        session.pop('user')
+        return 'Logged out successfully!'
+    else: 
+        return redirect('/')
 
 
 if __name__ == '__main__':
