dd1d54bbec7739a248c6cb9c1ef282bb24b1a0c9
Will10145 <beanoboylego.rbx@gmail.com>
2026-05-30 12:59:12 +0100

    Added basic firebase login

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
 from flask import *
 from dotenv import load_dotenv
+import os
+from authfunctions import *
 
 load_dotenv()
+
 app = Flask(__name__)
+
+app.secret_key = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def home():
     return render_template(
         'index.html',
-        styles=url_for('static', filename='dist/output.css'),
+        styles=url_for('static', filename='tailwindstyles.css'),
         plantimg=url_for('static', filename='plant.png')
         )
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if('user' in session):
+        return f"Hi, {session['user']}"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            login_email_password(email,password)
+            session['user'] = email
+        except:
+            return 'Failed to login'
+
     return render_template(
         'login.html',
-        styles=url_for('static', filename='tailwindstyles.css'),
+        styles=url_for('static', filename='login-style.css'),
         )
 
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
